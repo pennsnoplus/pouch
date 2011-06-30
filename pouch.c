18ba9e071074584be78f1a844fc2aef425408fb8
Peter Downs <peter.l.downs@gmail.com>
2011-06-30 13:07:27 -0400

    Finished the basic API structure for pouch.
    Everything compiles fine, but I haven't moved the main
    loop over to the new API. We'll see how it goes.

--- pouch.c
+++ pouch.c
@@ -4,6 +4,13 @@
 
 #include <curl/curl.h>
 
+#define GET "GET"
+#define PUT "PUT"
+#define POST "POST"
+#define HEAD "HEAD"
+#define COPY "COPY"
+#define DELETE "DELETE"
+
 typedef struct pouch_pkt {
 	char *data;
 	char *offset;
@@ -15,6 +22,70 @@ typedef struct pouch_resp {
 	CURLcode curlcode;
 } pouch_resp;
 
+typedef struct pouch_request {
+	char *method;		// HTTP method
+	char *url;	// Destination (e.g., "http://127.0.0.1:5984/test");
+	CURLcode curlcode;	// CURL request status holder
+	pouch_pkt req;		// holds data to be sent
+	pouch_pkt resp;		// holds response
+} pouch_request;
+
+pouch_request *pouch_request_new(void){
+	pouch_request *pr = calloc(1, sizeof(pouch_request));
+	pr->resp.data = NULL;	// empty the response buffer
+	pr->resp.size = 0;
+	
+	// initialize the data to be sent as nothing
+	pr->req.data = (char *)malloc(1);
+	memcpy(pr->req.data, 0, 1);
+	pr->req.offset = pr->req.data;
+	pr->req.size = 0;
+	return pr;
+}
+/*
+		if(data && strlen(data) > 0) {
+			send_pkt.data = (char *)malloc(strlen(data)+1);
+			memcpy(send_pkt.data, data, strlen(data)+1);
+			send_pkt.offset = send_pkt.data;
+			send_pkt.size = strlen(data)+1;
+*/
+pouch_request *pouch_request_set_method(pouch_request *pr, char *method) {
+	size_t length = strlen(method)+1; // include '\0' terminator
+	if(pr->method)
+		free(pr->method);
+	pr->method = (char *)malloc(length); // allocate space
+	memcpy(pr->method, method, length);	 // copy the method
+	return pr;
+}
+pouch_request *pouch_request_set_url(pouch_request *pr, char *url) {
+	size_t length = strlen(url)+1; // include '\0' terminator
+	if(pr->url)
+		free(pr->url);
+	pr->url = (char *)malloc(length);
+	memcpy(pr->url, url, length);
+	return pr;
+}
+pouch_request *pouch_request_set_data(pouch_request *pr, char *str) {
+	size_t length = strlen(str)+1; // include the '\0' terminator
+	if(pr->req.data)
+		free(pr->req.data);
+	pr->req.data = (char *)malloc(length);
+	memcpy(pr->req.data, str, length);
+	pr->req.offset = pr->req.data;
+	return pr;
+}
+
+void pouch_free_request(pouch_request *pr) {
+	if (pr->resp.data)
+		free(pr->resp.data);
+	if (pr->resp.offset)
+		free(pr->resp.offset);
+	if (pr->req.data)
+		free(pr->req.data);
+	if (pr->req.offset)
+		free(pr->req.offset);
+}
+
 size_t recv_data_callback(char *ptr, size_t size, size_t nmemb, void *data){
 	size_t ptrsize = nmemb*size;
 	pouch_pkt *pkt = (pouch_pkt *)data;
@@ -23,7 +94,7 @@ size_t recv_data_callback(char *ptr, size_t size, size_t nmemb, void *data){
 	if (pkt->data) { // realloc was successful
 		memcpy(&(pkt->data[pkt->size]), ptr, ptrsize); // append new data
 		pkt->size += ptrsize;
-		pkt->data[pkt->size] = '\0'; //TODO: why not '\0'?
+		pkt->data[pkt->size] = '\0';
 	}
 	else {
 		fprintf(stderr, "recv_data_callback: realloc failed\n");
@@ -44,7 +115,7 @@ size_t send_data_callback(void *ptr, size_t size, size_t nmemb, void *data){
 	return 0;
 }
 
-pouch_resp pouch_curl(char *urlstring, char *method, char *data){
+pouch_resp pouch_curl(char *url, char *method, char *data){
 	CURL *curl;
 	CURLcode ret;
 	struct curl_slist *headers = NULL;
@@ -60,14 +131,14 @@ pouch_resp pouch_curl(char *urlstring, char *method, char *data){
 	if(curl){
 		// setup the CURL object/request
 		curl_easy_setopt(curl, CURLOPT_USERAGENT, "pouch/0.1");
-		curl_easy_setopt(curl, CURLOPT_URL, urlstring);
+		curl_easy_setopt(curl, CURLOPT_URL, url);
 		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, method);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, recv_data_callback);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&recv_pkt);
 
 		// print the request
 		printf("libcurl request:\n");
-		printf("\t%s : %s\n", method, urlstring);
+		printf("\t%s : %s\n", method, url);
 		if(data && strlen(data) > 0) {
 			send_pkt.data = (char *)malloc(strlen(data)+1);
 			memcpy(send_pkt.data, data, strlen(data)+1);
@@ -108,8 +179,13 @@ pouch_resp pouch_curl(char *urlstring, char *method, char *data){
 
 int main(int argc, char* argv[]){
 	char *data = NULL;
-	pouch_resp couchdata = pouch_curl("http://127.0.0.1:5984/test/", "GET", data);
+	pouch_resp couchdata = pouch_curl("http://127.0.0.1:5984", "GET", data);
+	printf("CURL_code: %d\n", couchdata.curlcode);
+	printf("Response length: %d\n", (int)couchdata.pkt.size);
+	printf("Response data: %s\n", couchdata.pkt.data);
+
 	free(couchdata.pkt.data);
 	free(couchdata.pkt.offset);
+	
 	return 0;
 }
